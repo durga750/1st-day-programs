e15f80bfe5da30c7433632e6ed1df90564e4b2ad
durga750 <78526756+durga750@users.noreply.github.com>
2021-02-06 15:48:11 +0530

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Matrix
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] a = { { 1, 2, 3}, { 4, 5, 6 }, { 7, 8, 9} };
+           
+            for (int i = 0; i <a.GetLength(0); i++)
+            {
+                for (int j = 0; j <a. GetLength(1); j++)
+                {
+                
+                    Console.Write(a[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
