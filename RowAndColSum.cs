485fe41a7f8c8a1d9505573123351978ee2f81b3
durga750 <78526756+durga750@users.noreply.github.com>
2021-02-08 15:31:26 +0530

    Add files via upload

---
+++ RowAndColSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsParactice
+{
+    class RowAndColSum
+    {
+        static void Main(string[] args)
+        {
+           
+            int[,] a = { { 1, 2, 3 }, { 4, 5, 6 }, { 1, 2, 3 } };
+            for (int i = 0; i < 3; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(a[i,j]+"\t");
+                    sum = sum + a[i,j];
+                  
+                }
+                Console.WriteLine(sum);
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    sum = sum + a[j,i];
+
+                }
+                Console.Write(sum+"\t");
+            }
+            }
+    }
+}
